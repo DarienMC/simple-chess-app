98fb3cbdcaa97e0b18ac60be4d8f04b0afe8ebad
Darien MC <darienmusecharbonneau@gmail.com>
2023-01-08 16:37:39 -0700

    Update Game.py
    update core game loop

--- src/model/Game.py
+++ src/model/Game.py
@@ -10,11 +10,25 @@ class GameState(Enum):
 
 class Game:
     state = GameState.IN_PROGRESS
-    current_position = Position()
+    current_position = Position
     moves = None
-    
-    def play(self):
+
+    def change_to_subsequent_position(self, move):
         pass
+
+    def play(self):
+        while self.state is GameState.IN_PROGRESS:
+            if self.current_position.legal_moves is not None:
+                move = input(f'{players[current_position.turn_index]} to move: ')
+                self.change_to_subsequent_position(move)
+            else:
+                if self.current_position.in_check:
+                    if self.current_position.turn_index is 0:
+                        self.state = GameState.WHITE_WINS
+                    else:
+                        self.state = GameState.BLACK_WINS
+                else:
+                    self.state = GameState.DRAW
 
     def __init__(self, players):
         self.players = players
