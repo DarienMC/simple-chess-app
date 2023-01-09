abda7c2c7128c5aaec9f22cffaab75b8fbf70d2d
Darien MC <darienmusecharbonneau@gmail.com>
2023-01-08 18:49:04 -0700

    Update Piece.py
    Added a method that retrieves the unicode or letter representation of the piece if applicable.

--- src/model/Piece.py
+++ src/model/Piece.py
@@ -9,35 +9,110 @@ class Piece(ABC):
     def calculate_legal_moves(self, position):
         pass
 
+    @abstractmethod
+    def get_unicode(self):
+        pass
+
+    @abstractmethod
+    def get_abbreviation(self):
+        pass
+
     # Constructor
     def __init__(self, color, occupied_square):
         self.color = color
         self.occupied_square = occupied_square
 
+
 class King(Piece):
     def calculate_legal_moves(self, position):
         pass
+
+    def get_unicode(self):
+        if self.color is 0:
+            return '♔'
+        else:
+            return '♚'
+
+    def get_abbreviation(self):
+        return 'K'
+
 
 class Queen(Piece):
     def calculate_legal_moves(self, position):
         pass
 
+    def get_unicode(self):
+        if self.color is 0:
+            return '♕'
+        else:
+            return '♛'
+
+    def get_abbreviation(self):
+        return 'Q'
+
+
 class Rook(Piece):
     def calculate_legal_moves(self, position):
         pass
+
+    def get_unicode(self):
+        if self.color is 0:
+            return '♖'
+        else:
+            return '♜'
+
+    def get_abbreviation(self):
+        return 'R'
+
 
 class Bishop(Piece):
     def calculate_legal_moves(self, position):
         pass
 
+    def get_unicode(self):
+        if self.color is 0:
+            return '♗'
+        else:
+            return '♝'
+
+    def get_abbreviation(self):
+        return 'B'
+
+
 class Knight(Piece):
     def calculate_legal_moves(self, position):
         pass
+
+    def get_unicode(self):
+        if self.color is 0:
+            return '♘'
+        else:
+            return '♞'
+
+    def get_abbreviation(self):
+        return 'N'
+
 
 class Pawn(Piece):
     def calculate_legal_moves(self, position):
         pass
 
+    def get_unicode(self):
+        if self.color is 0:
+            return '♙'
+        else:
+            return '♟︎'
+
+    def get_abbreviation(self):
+        return 'P'
+
+
 class ShadowPawn(Piece):
     def calculate_legal_moves(self, position):
+        return None
+
+    def get_unicode(self):
+        return None
+
+    def get_abbreviation(self):
         return None
