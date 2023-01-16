5d3441255c0ae33353806994ca055253208704d3
Darien MC <darienmusecharbonneau@gmail.com>
2023-01-16 15:13:15 -0700

    Method to print board moved from Game class to Position class

--- src/model/Game.py
+++ src/model/Game.py
@@ -10,23 +10,6 @@ class GameState(Enum):
     DRAW = 4
 
 
-def print_current_position_as_text():
-    print(
-        '    a   b   c   d   e   f   g   h    '
-        '  =================================  '
-        '8 | R | N | B | Q | K | B | N | R | 8'
-        '7 | P | P | P | P | P | P | P | P | 7'
-        '6 |   |   |   |   |   |   |   |   | 6'
-        '5 |   |   |   |   |   |   |   |   | 5'
-        '4 |   |   |   |   |   |   |   |   | 4'
-        '3 |   |   |   |   |   |   |   |   | 3'
-        '2 | P | P | P | P | P | P | P | P | 2'
-        '1 | R | N | B | Q | K | B | N | R | 1'
-        '  =================================  '
-        '    a   b   c   d   e   f   g   h    '
-    )
-
-
 def change_to_subsequent_position():
     return Position
 
