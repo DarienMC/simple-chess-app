fe509e043e32040ff1e1a11af98117f7ba7eeba5
Darien MC <darienmusecharbonneau@gmail.com>
2023-01-16 15:17:56 -0700

    Update Player.py

--- src/model/Player.py
+++ src/model/Player.py
@@ -1,5 +1,21 @@
 class Player:
+    # Class attributes
     __elo = 1000
 
+    # Constructor
     def __init__(self, name):
         self.__name = name
+
+    # Getters & Setters
+    def get_elo(self):
+        return self.__elo
+
+    def set_elo(self, elo):
+        self.__elo = elo
+
+    def get_name(self):
+        return self.__name
+
+    def set_elo(self, name):
+        self.__name = name
+
